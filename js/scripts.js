3a31667d4738172dc07c1ebf048a7c7ebcba8bec
Kendra <kendra@coffeeringsdesign.com>
2018-08-10 14:15:54 -0700

    fix all issues with if and else leading to correct answer

--- js/scripts.js
+++ js/scripts.js
@@ -7,49 +7,55 @@ $(document).ready(function() {
 
   $("#questionsForm").submit(function(event) {
     event.preventDefault();
-    var thisisIt = $("#gender").val();
     $(".questions").hide();
-      if (thisisIt === "a") {
+
+    var choicefrontBack = $("#frontVsbackend").val();
+      if (choicefrontBack === "front") {
       $("#visualVsdata").show();
-    };
+    } else (choicefrontBack === "back") {
+      $("#behindorSitesdata").show();
+    }
+    var choiceVisData =
+    $("#visualVsdata").val();
+      if (choiceVisData === "visual") {
+      $("#sites").show();
+    } else (choiceVisData === "data") {
+      $("#language").show();
+    }
+    var choiceScenesData =
+    $("#behindorSitesdata").val()
+      if (choiceScenesData === "datasites") {
+      $("#ruby").show();
+    } else (choiceScenesData === "behindscenes") {
+      $("#whattoBuild").show();
+    }
+    var choicetoBuild =
+    $("#whattoBuild")val();
+      if (choicetoBuild === "sharepoint") {
+      $("#csharp").show();
+    } else (choicetoBuild === "stack") {
+      $("#csharp").show();
+    }
+    var likeProblems =
+    $("#problemsolving")val();
+      if (likeProblems === "nayproblems") {
+      $("#css").show();
+    } else (likeProblems === "yayproblems") {
+      $("#ruby").show();
+    }
+    var whichLanguage =
+    $("#language").val();
+      if (whichLanguage === "object") {
+      $("#ruby").show();
+    } else (whichLanguage === "traditional") {
+      $("#css").show();
+    }
+    var whichSites =
+    $("#sites").val();
+      if (whichSites === "atlassian") {
+      $("#csharp").show();
+    } else (whichSites === "hulu") {
+      $("#ruby").show();
+    }
   });
 });
-
-
-
-
-    // $(".questions").hide();
-    //       if (choseBack)  {
-    //       $("#behindorSitesdata").show();
-    //     } else if (choseFront) {
-    //       $("#language").show();
-    //
-    //
-    //     } else if (choseFront) {
-    //       $("#language").show();
-    //     } else if (choseFront) {
-    //       $("#language").show();
-    //     } else if (choseFront) {
-    //       $("#language").show();
-    //     } else if (choseFront) {
-    //       $("#language").show();
-    //     } else if (choseFront) {
-    //       $("#language").show();
-
-
-
-
-
-
-
-
-    //
-    // if ( -----------------) {
-    //   $("#css").show();
-    // } else if (------------) {
-    //   $("#ruby").show();
-    // } else if (------------) {
-    //   $("cSharp").show();
-    // } else {
-    //   prompt("please try again");
-    // });
