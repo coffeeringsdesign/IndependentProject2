2936b71cbd0e8cc73fe819e5754f728c755e5bf0
Kendra <kendra@coffeeringsdesign.com>
2018-08-10 12:16:23 -0700

    rework questions and begin else if statements throughout

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,48 @@ $(document).ready(function() {
     var theirName = $("input#visitornameInput").val();
     $(".showthemtheirName").text(theirName);
 
+
+    var choseBack = $("#frontVsbackend").val;
+    var choseFront = $("#frontVsbackend").val;
+    var choseVisual = $("#visualVsdata").val;
+    var choseData = $("#visualVsdata").val;
+    var choseDatasites = $("#behindorSitesdata").val;
+    var choseBehind = $("#behindorSitesdata").val;
+    var choseShare = $("#whattoBuild").val;
+    var choseStack = $("#whattoBuild").val;
+    var choseYay = $("#problemsolving").val;
+    var choseNay = $("#problemsolving").val;
+    var choseObject = $("#language").val;
+    var choseTraditional = $("#language").val;
+    var choseAtlas = $("#sites").val;
+    var choseHulu = $("#sites").val;
+
+
+    $(".questions").hide();
+          if (choseBack)  {
+          $("#behindorSitesdata").show();
+        } else if (choseFront) {
+          $("#language").show();
+
+          
+        } else if (choseFront) {
+          $("#language").show();
+        } else if (choseFront) {
+          $("#language").show();
+        } else if (choseFront) {
+          $("#language").show();
+        } else if (choseFront) {
+          $("#language").show();
+        } else if (choseFront) {
+          $("#language").show();
+
+
+
+
+
+
+
+
     //
     // if ( -----------------) {
     //   $("#css").show();
