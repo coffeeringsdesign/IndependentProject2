1fa67949b02b0dfbac80cec77b3111c9a0c65350
Kendra <kendra@coffeeringsdesign.com>
2018-08-18 15:39:18 -0700

    move each question to their own form - fix all trigger issues

--- js/scripts.js
+++ js/scripts.js
@@ -2,43 +2,59 @@ $(document).ready(function() {
   $("#nameForm").submit(function(event) {
     event.preventDefault();
     var theirName = $("input#visitornameInput").val();
-    $(".showthemtheirName").text(theirName);
-    $("#question1").show();
+    $("#showthemtheirName").text(theirName);
+    $("#question1").hide().delay(2000).fadeIn(4000);
+    $("#nameQuestion").fadeIn();;
+    console.log(theirName);
   });
 
-  $("#questionsForm").click(function(event) {
+  $("#question1").submit(function(event) {
     event.preventDefault();
-    var choicefrontBack = $("#frontOrBackend").val();
-      if (choicefrontBack === "1") {
-      $('#question2').show();
-    } else if (choicefrontBack === "2") {
-      $('#question3').show();
+    var frontOrBackend = $("input[name='frontOrBackend']:checked").val();
+    if (frontOrBackend === "1") {
+    $("#question2").fadeIn();
+  } else if (frontOrBackend === "2") {
+    $("#question3").fadeIn();
+  }
+});
+
+  $("#question2").submit(function(event) {
+    event.preventDefault();
+    var visualOrData = $("input[name='visualOrData']:checked").val();
+    if (visualOrData === "1") {
+    $("#question4").fadeIn();
+  } else if (visualOrData === "2") {
+    $("#question5").fadeIn();
+  }
+  });
+
+  $("#question3").submit(function(event) {
+    event.preventDefault();
+    var likeProblems = $("input[name='problemsolving']:checked").val();
+    if (likeProblems === "1") {
+    $("#css").show();
+  } else if (likeProblems === "2") {
+    $("#ruby").show();
     }
-    var choiceVisData = $("#visualOrData").val();
-      if (choiceVisData === "1") {
-      $('#question4').show();
-    } else if (choiceVisData === "2") {
-      $('#question5').show();
-    }
-    var whichSites = $("#sites").val();
+    });
+
+  $("#question4").submit(function(event) {
+    event.preventDefault();
+    var whichLanguage = $("input[name='language']:checked").val();
+    if (whichLanguage === "1") {
+    $("#ruby").show();
+  } else if (whichLanguage === "2") {
+    $("#css").show();
+  }
+});
+
+  $("#question5").submit(function(event) {
+    event.preventDefault();
+    var whichSites = $("input[name='sites']:checked").val();
       if (whichSites === "1") {
-      $("#cSharp").show();
+      $("#cSharp").fadeIn();
     } else if (whichSites === "2") {
-      $("#ruby").show();
+      $("#ruby").fadeIn();
     }
-    var whichLanguage = $("#language").val();
-      if (whichLanguage === "1") {
-      $("#ruby").show();
-    } else if (whichLanguage === "2") {
-      $("#css").show();
-    }
-    var likeProblems = $("#problemsolving").val();
-      if (likeProblems === "1") {
-      $("#css").show();
-    } else if (likeProblems === "2") {
-      $("#ruby").show();
-      }
-console.log(choicefrontBack);
-  event.preventDefault();
   });
 });
